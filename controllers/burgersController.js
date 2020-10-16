bebe96f43036e8f7e019d7a661d1105780370ccb
Paul Murray <paulmurray85@gmail.com>
2020-10-16 15:11:28 -0700

    add sample html page for testing

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,4 +5,19 @@ const burger = require("../models/burger.js");
 
 const router = express.Router();
 
+// this creates all the routes needed 
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.create([req.body.name], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
 module.exports = router;
